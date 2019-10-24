b619bd35ea434ede32ed641b9de81867fdeb4471
Alex <alexander.markham@lmu.de>
2019-10-24 12:16:38 +0200

    rewrote UDG class to take adj matrix as input; started adding aux data struct and reduced graph class

--- medil/graph.py
+++ medil/graph.py
@@ -9,29 +9,34 @@ import numpy as np
 
 
 class UndirectedDependenceGraph(object):
-    def __init__(self, num_vertices):
-        # num_vertices is an int
-        self.num_vertices = num_vertices
-        self.adj_matrix = np.zeros((num_vertices, num_vertices), int)
-        self.num_edges = 0
 
+    def __init__(self, adj_matrix):
+        # doesn't behave well unless input is nparray;
+        self.adj_matrix = adj_matrix
+        self.num_vertices = len(adj_matrix)
+        self.num_edges = adj_matrix.sum() // 2
 
     def add_edges(self, edges):
-        # doesn't behave well unless input is nparray;
         v_1s = edges[:, 0]
         v_2s = edges[:, 1]
         self.adj_matrix[v_1s, v_2s] = 1
         self.adj_matrix[v_2s, v_1s] = 1
         self.num_edges = self.adj_matrix.sum() // 2
 
-
     def rm_edges(self, edges):
-        # doesn't behave well unless input is nparray;
         v_1s = edges[:, 0]
         v_2s = edges[:, 1]
         self.adj_matrix[v_1s, v_2s] = 0
         self.adj_matrix[v_2s, v_1s] = 0
         self.num_edges = self.adj_matrix.sum() // 2
+
+    def make_aux(self):
+        self.
+        self.
+
+class ReducedUndDepGraph(UndirectedDependenceGraph):
+    pass
+
 
 
 # class minMCM(object):
@@ -40,3 +45,4 @@ class UndirectedDependenceGraph(object):
 
 # class MCM(object):
 # implement as large DAG adj matrix over L and M, or smaller bigraph for L-M connections and DAG adj matirx for L->L connections
+
