fixed minor max_intersection_num calculation bug

medil/ecc_algorithms.py
@@ -17,7 +17,7 @@ def find_clique_min_cover(graph, verbose=False):
         if max_intersect_num < graph.num_edges:
             p = graph.n_choose_2(graph.num_vertices) - graph.num_edges
             t = int(np.sqrt(p))
-            max_intersect_num = p + t            
+            max_intersect_num = p + t if p > 0 else 0
         print("solution has at most {} cliques.".format(max_intersect_num))
     while the_cover is None:
         if True: