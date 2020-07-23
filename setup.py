fcad402c61382aec954d30e313cfa6ba04a4c1b8
Alex Markham <alexander.markham@univie.ac.at>
2020-07-23 15:12:32 +0200

    updated setup.py requires and added extra_requires

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from distutils.core import setup
 setup(name='medil',
       version='0.4.0',
       packages=['medil'],
-      install_requires=['dcor'])
+      install_requires=['numpy']
+      extras_require={
+          'dcor': ['dcor'],
+          'GAN' : ['pytorch'],
+          'visualization' : ['matplotlib', 'networkx']
+      }
+)
