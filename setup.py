26a96ee5d52166d887d5c5b31aeb0a6ccac881c8
alex <alexander.markham@univie.ac.at>
2020-07-23 19:34:12 +0200

    changed setup.py to read v0.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='medil',
-      version='0.4.0',
+      version='0.5.0',
       packages=['medil'],
       install_requires=['numpy'],
       extras_require={
