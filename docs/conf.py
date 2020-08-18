aef7781e053e594a3e0daa15f07a18a909feaa6a
Alex Markham <alexander.markham@univie.ac.at>
2020-08-18 15:20:44 +0200

    added autodoc sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Alex Markham'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['m2r2']
+extensions = ['m2r2', 'sphinx.ext.autodoc', 'sphinx.ext.napolean']
 source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
