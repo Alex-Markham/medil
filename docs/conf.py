01331d0c1be97e31c6c00a09068f96ab945a92e8
Alex Markham <alex.markham@univie.ac.at>
2021-03-12 17:42:28 +0100

    updated conf

--- docs/conf.py
+++ docs/conf.py
@@ -12,22 +12,23 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
-project = 'MeDIL'
-copyright = '2019–2020, Alex Markham'
-author = 'Alex Markham'
+project = "MeDIL"
+copyright = "2019–2021, Alex Markham"
+author = "Alex Markham"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-with open('../setup.py') as f:
+with open("../setup.py") as f:
     lines = f.readlines()
     v_line = lines[9]
     version = v_line[15:-3]
@@ -37,28 +38,46 @@ with open('../setup.py') as f:
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['m2r2', 'sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'sphinx_rtd_theme', 'sphinx.ext.viewcode', 'sphinx.ext.mathjax', 'sphinxcontrib.bibtex']
-source_suffix = ['.rst', '.md']
+extensions = [
+    "m2r2",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.mathjax",
+    "sphinxcontrib.bibtex",
+]
+source_suffix = [".rst", ".md"]
+
+bibtex_bibfiles = ["references.bib"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
-autosummary_mock_imports = ['numpy', 'matplotlib', 'seaborn', 'networkx', 'torch', 'pytorch_lightning']
+autosummary_mock_imports = [
+    "numpy",
+    "matplotlib",
+    "seaborn",
+    "networkx",
+    "torch",
+    "pytorch_lightning",
+]
 autosummary_generate = True
 
 # A list of prefixs that are ignored when creating the module index. (new in Sphinx 0.6)
 modindex_common_prefix = ["medil."]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -66,12 +85,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # not sure why this is needed, since I used sphinx defaults
-master_doc = 'index'
+master_doc = "index"
