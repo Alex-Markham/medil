78f206a964ffbaa3cca1b211cd3f346598c8fa96
Alex Markham <alex.markham@causal.dev>
2022-10-18 12:48:06 +0000

    Grues

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,12 +5,23 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Unreleased ##
-- add HSIC test
 - implement AM clique cover
+- implement ECC heuristics
 
 ### In progress ###
 - causal clustering
-	
+- causal abstraction
+- GUES: improved GES using unconditional equivalence classes (UECs)
+- improving GAN performance and theory
+
+## [0.7.0] - 2022-10-18 ##
+### Changed ###
+- updated licence to CNPLv7+
+- updated description in `setup.py` and readme to reflect expanded focus of the package
+
+### Added ###
+- `grues` submodule implementing an MCMC-based method for UEC learning
+
 ## [0.6.0] - 2021-06-07 ##
 ### Changed ###
 - licences from AGPLv3+ to CNPLv6+	
--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def long_description():
 
 setup(
     name="medil",
-    version="0.6.0",
+    version="0.7.0",
     author="Alex Markham",
     author_email="alex.markham@causal.dev",
-    description="This package is for causal inference, focusing on the Measurement Dependence Inducing Latent (MeDIL) Causal Model framework.",
+    description="This package is for causal modeling, originally focusing on the measurement dependence inducing latent (MeDIL) causal model framework, but now including more general methods for causal discovery and inference.",
     long_description_content_type="text/markdown",
     long_description=long_description(),
-    license="Cooperative Non-Violent Public License v6 or later (CNPLv6+)",
+    license="Cooperative Non-Violent Public License v7 or later (CNPLv7+)",
     packages=["medil"],
     install_requires=["numpy"],
     extras_require={
