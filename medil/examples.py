3530d351078d9926f1799df43547313ccecc883c
Alex Markham <markham@kth.se>
2022-10-27 16:11:35 +0200

    added examples and cleaned formatting

--- medil/examples.py
+++ medil/examples.py
@@ -4,6 +4,7 @@ import numpy as np
 
 class ExampleUDGAndMCM(object):
     r"""Example consisting of a description, UDG, and MCM"""
+
     def __init__(self, description):
         self.description = description
         self.UDG = None
@@ -17,21 +18,12 @@ class ExampleUDGAndMCM(object):
 
 
 simple_M = ExampleUDGAndMCM("M-shaped mcm, with 2 latent- and 3 measurement-variables")
-simple_M.add_udg(
-    [
-        [1, 1, 0],
-        [1, 1, 1],
-        [0, 1, 1]
-    ]
-)
-simple_M.add_mcm(
-    [
-        [0, 1, 1],
-        [1, 1, 0]
-    ]
-)
+simple_M.add_udg([[1, 1, 0], [1, 1, 1], [0, 1, 1]])
+simple_M.add_mcm([[0, 1, 1], [1, 1, 0]])
 
-triangle = ExampleUDGAndMCM("triangle example, where minECC differs from set of all maximal cliques")
+triangle = ExampleUDGAndMCM(
+    "triangle example, where minECC differs from set of all maximal cliques"
+)
 triangle.add_udg(
     [
         [1, 1, 1, 0, 0, 0],
@@ -42,15 +34,11 @@ triangle.add_udg(
         [0, 0, 1, 0, 1, 1],
     ]
 )
-triangle.add_mcm(
-    [
-        [0, 0, 1, 0, 1, 1],
-        [0, 1, 0, 1, 1, 0],
-        [1, 1, 1, 0, 0, 0]
-    ]
-)
+triangle.add_mcm([[0, 0, 1, 0, 1, 1], [0, 1, 0, 1, 1, 0], [1, 1, 1, 0, 0, 0]])
 
-more_latents = ExampleUDGAndMCM("example where number of latent variables is larger than number of measurement variables")
+more_latents = ExampleUDGAndMCM(
+    "example where number of latent variables is larger than number of measurement variables"
+)
 more_latents.add_udg(
     [
         [1, 1, 0, 1, 0, 1],
@@ -74,7 +62,9 @@ more_latents.add_mcm(
     ]
 )
 
-am_cm_diff = ExampleUDGAndMCM("example where multiple ECCs are possible depending on edge minimal vs vertex minimal")
+am_cm_diff = ExampleUDGAndMCM(
+    "example where multiple ECCs are possible depending on edge minimal vs vertex minimal"
+)
 am_cm_diff.add_udg(
     [
         [1, 1, 1, 1, 1, 0, 1, 0],
@@ -97,7 +87,33 @@ am_cm_diff.add_mcm(
     ]
 )
 
-examples = (simple_M, triangle, more_latents, am_cm_diff)
+max_assignment_num = ExampleUDGAndMCM(
+    "example where an edge has maximum assignment number, n - 2"
+)
+max_assignment_num.add_udg(
+    [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0, 0, 1, 0],
+        [1, 1, 0, 0, 0, 0, 0, 1],
+    ]
+)
+max_assignment_num.add_mcm(
+    [
+        [1, 1, 1, 0, 0, 0, 0, 0],
+        [1, 1, 0, 1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0, 0, 1, 0],
+        [1, 1, 0, 0, 0, 0, 0, 1],
+    ]
+)
+
+examples = (simple_M, triangle, more_latents, am_cm_diff, max_assignment_num)
 
 
 ## Data
