0beb395fadd6ad5bd52952090432170cf74ba676
Alex Markham <markham@kth.se>
2023-05-04 12:16:37 +0200

    commented out soon-to-be-removed test file

--- tests/test_simulate.py
+++ tests/test_simulate.py
@@ -1,11 +1,12 @@
 import numpy as np
 from medil.simulate import MicroModel as mm
-from medil.grues import InputData as data
 
-mic = mm(5, 0.5)
-samp = mic.sample(1000)
-grues = data(samp)
-grues.mcmc(prior=(2, 3))
+# from medil.grues import InputData as data
 
-grues.optimal_uec
-mic.uec
+# mic = mm(5, 0.5)
+# samp = mic.sample(1000)
+# grues = data(samp)
+# grues.mcmc(prior=(2, 3))
+
+# grues.optimal_uec
+# mic.uec
