5e08b0594927f2ae2dec8ee1734a72b30e187a0e
Alex Markham <markham@kth.se>
2024-03-12 17:23:52 +0100

    add notes and start wrapper func

--- medil/__init__.py
+++ medil/__init__.py
@@ -1,4 +1,6 @@
 """This package is for causal inference, focusing on the Measurement
 Dependence Inducing Latent (MeDIL) Causal Model framework."""
+from numpy import median
 from .functional_MCM import MedilCausalModel
-from .functional_MCM import NeuroCausalFactorAnalysis
+
+ncfa = MedilCausalModel(parameterization="vae")
