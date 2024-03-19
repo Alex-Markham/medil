a3d29e3bee8e845c9c106744f8669e7234139e1b
Alex Markham <markham@kth.se>
2024-03-19 09:25:50 +0100

    sketched sample submodule

---
+++ medil/sample.py
@@ -0,0 +1,16 @@
+"""Randomly generate a parameterized minimum MeDIL causal model or associated graph."""
+from numpy.random import default_rng
+
+from .models import MedilCausalModel
+
+
+def mcm():
+    pass
+
+
+def udg():
+    pass
+
+
+def biadj():
+    pass
