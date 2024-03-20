5d85e4434cc2fc076f88c67d55859d65d8fb9f7c
Alex Markham <markham@kth.se>
2024-03-20 10:54:34 +0100

    move MCM and NCFA objecs to `medil` namespace

--- medil/__init__.py
+++ medil/__init__.py
@@ -1,6 +1,3 @@
 """This package is for causal inference, focusing on the Measurement
 Dependence Inducing Latent (MeDIL) Causal Model framework."""
-from numpy import median
-from .functional_MCM import MedilCausalModel
-
-ncfa = MedilCausalModel(parameterization="vae")
+from .models import MedilCausalModel, NeuroCausalFactorAnalysis
