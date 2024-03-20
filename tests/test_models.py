4887bdfc41855a161eaa28ae371fbe4120903b43
Alex Markham <markham@kth.se>
2024-03-20 14:45:15 +0100

    add basic test_sample for MedilCausalModel

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,15 +5,17 @@ from medil.models import MedilCausalModel, NeuroCausalFactorAnalysis
 
 
 class TestMedilCausalModel:
-    def test_sample(self):
+    def __init__(self):
         biadj = np.zeros((2, 3), bool)
         biadj[[0, 0, 1, 1], [0, 1, 1, 2]] = True
-
         mcm = MedilCausalModel(biadj)
         mcm.biadj_weights = biadj.astype(float)
         mcm.error_means = np.zeros(3)
         mcm.error_variances = np.ones(3)
-        s = mcm.sample(100000)
+        self.basic_mcm = mcm
+
+    def test_sample(self):
+        s = self.basic_mcm.sample(100000)
         assert np.allclose(s.mean(0), mcm.error_means, atol=0.02)
 
 
