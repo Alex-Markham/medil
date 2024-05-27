766e7669a7806d7d3c7fc3141b4c17a3491f392e
Alex Markham <markham@kth.se>
2024-05-27 15:58:46 +0200

    refactoring api

--- medil/__init__.py
+++ medil/__init__.py
@@ -1,3 +1,3 @@
 """This package is for causal inference, focusing on the Measurement
 Dependence Inducing Latent (MeDIL) Causal Model framework."""
-from .models import MedilCausalModel, NeuroCausalFactorAnalysis
+from .models import GaussianMCM, NeuroCausalFactorAnalysis
