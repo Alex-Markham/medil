d292dbb5acdc8e914899141c9552b6fc25a3631b
Alex Markham <alex.markham@causal.dev>
2024-10-11 12:56:40 +0200

    fix: bug in ecc_algorithms.find_heuristic_1pc

--- medil/ecc_algorithms.py
+++ medil/ecc_algorithms.py
@@ -1,5 +1,5 @@
 """Implementations of edge clique clover finding algorithms."""
-import subprocess, os, shutil
+
 from collections import deque
 
 import numpy as np
@@ -246,7 +246,7 @@ def find_heuristic_1pc(graph):
 
     uncovered_edges = deque(
         {
-            edge
+            tuple(edge)
             for edge in np.argwhere(np.triu(graph, 1))
             if not np.logical_and(the_cover[:, edge[0]], the_cover[:, edge[1]]).any()
         }
