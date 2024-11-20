ae39afb881ef7143dd17dfe8aa1ac5331149956e
Alex Markham <alex.markham@causal.dev>
2024-11-20 16:02:48 +0100

    add raw shd and sfd back

--- medil/evaluate.py
+++ medil/evaluate.py
@@ -2,8 +2,19 @@ import numpy as np
 import numpy.typing as npt
 
 
-def nsfd(true_biadj: npt.NDArray, predicted_biadj: npt.NDArray) -> float:
-    """Measure distance between predicted and true and structures.
+def sfd(
+    true_biadj: npt.NDArray,
+    predicted_biadj: npt.NDArray,
+    to_return: str = "raw",
+) -> int | float | tuple[int, float]:
+    """Structural Frobenius distance sums difference of latent parents.
+
+    For a binary biadjacency matrix B, consider U = B'B, where U_ij
+    counts the number of parents nodes i and j have in common (so U_ii
+    is the assignment number, and diag(U) is a sufficient statistic
+    for the graph under the 1-pure-child assumption). sfd(B_1, B_2) is
+    the sum of the differences between U_ij for B_1 and B_2 (without
+    double-counting for U_ji).
 
     Parameters
     ----------
@@ -16,11 +27,18 @@ def nsfd(true_biadj: npt.NDArray, predicted_biadj: npt.NDArray) -> float:
     """
     true_biadj = true_biadj.astype(int)
     true_wtd_ug = true_biadj.T @ true_biadj
-    true_zeros = np.where(true_wtd_ug == 0)
-    true_wtd_ug[true_zeros] = -1
 
     predicted_biadj = predicted_biadj.astype(int)
     predicted_wtd_ug = predicted_biadj.T @ predicted_biadj
+
+    sfd = np.abs(np.triu(true_wtd_ug - predicted_wtd_ug)).sum()
+
+    if to_return == "raw":
+        return sfd
+
+    true_zeros = np.where(true_wtd_ug == 0)
+    true_wtd_ug[true_zeros] = -1
+
     predicted_zeros = np.where(predicted_wtd_ug == 0)
     predicted_wtd_ug[predicted_zeros] = -1
 
@@ -32,16 +50,23 @@ def nsfd(true_biadj: npt.NDArray, predicted_biadj: npt.NDArray) -> float:
 
     nsfd = np.arccos(similarity / cosin_normalizer) / np.pi
 
-    return nsfd
+    match to_return:
+        case "normalized":
+            return nsfd
+        case "both":
+            return sfd, nsfd
+        case _:
+            raise ValueError("`to_return` should be 'raw', 'normalized', or 'both'")
 
 
-def nshd(
+def shd(
     true_biadj: npt.NDArray,
     *,
     predicted_biadj: npt.NDArray = np.array([]),
     predicted_adj: npt.NDArray = np.array([]),
-) -> float:
-    """Measure distance between predicted and true and structures.
+    to_return: str = "raw",
+) -> int | float | tuple[int, float]:
+    """Structural Hamming distance counts number of incorrect arrowheads/tails.
 
     Parameters
     ----------
@@ -63,9 +88,20 @@ def nshd(
     ug = recover_ug(true_biadj)
 
     shd = np.logical_xor(ug, predicted_adj).sum()
+
+    if to_return == "raw":
+        return shd
+
     n = len(ug)
     nshd = shd / (n**2 - n)
-    return nshd
+
+    match to_return:
+        case "normalized":
+            return nshd
+        case "both":
+            return shd, nshd
+        case _:
+            raise ValueError("`to_return` should be 'raw', 'normalized', or 'both'")
 
 
 def recover_ug(biadj_mat: npt.NDArray) -> npt.NDArray:
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from medil.evaluate import nsfd, nshd
+from medil.evaluate import sfd, shd
 
 
 def true_mcm():
@@ -25,15 +25,25 @@ def est_mcm2():
 
 def test_nshd():
     with pytest.raises(ValueError):
-        nshd([1])
+        shd([1])
     with pytest.raises(ValueError):
-        nshd([1], predicted_biadj=[1], predicted_adj=[1])
-    assert nshd(true_mcm(), predicted_biadj=est_mcm1()) == 1 / 6
-    assert nshd(true_mcm(), predicted_biadj=est_mcm2()) == 1 / 6
-    assert nshd(est_mcm1(), predicted_biadj=est_mcm2()) == 1 / 3
+        shd([1], predicted_biadj=[1], predicted_adj=[1])
+    with pytest.raises(ValueError):
+        shd(true_mcm(), predicted_biadj=est_mcm1(), to_return="nope")
+    assert shd(true_mcm(), predicted_biadj=est_mcm1(), to_return="raw") == 2
+    assert shd(true_mcm(), predicted_biadj=est_mcm1(), to_return="normalized") == 2 / 12
+    assert shd(true_mcm(), predicted_biadj=est_mcm1(), to_return="both") == (2, 2 / 12)
+    assert shd(true_mcm(), predicted_biadj=est_mcm2()) == 2
+    assert shd(est_mcm1(), predicted_biadj=est_mcm2()) == 4
 
 
 def test_nsfd():
-    assert nsfd(true_mcm(), est_mcm1()) == 0.21501600287975522
-    assert nsfd(true_mcm(), est_mcm2()) == 0.27774888397299874
-    assert nsfd(est_mcm1(), est_mcm2()) == 0.29238200914015666
+    with pytest.raises(ValueError):
+        sfd(true_mcm(), est_mcm1(), to_return="nope")
+    assert sfd(true_mcm(), est_mcm1(), to_return="raw") == 3
+    assert sfd(true_mcm(), est_mcm2(), to_return="raw") == 4
+    assert sfd(est_mcm1(), est_mcm2(), to_return="raw") == 5
+    assert sfd(true_mcm(), est_mcm1(), to_return="normalized") == 0.21501600287975522
+    assert sfd(true_mcm(), est_mcm2(), to_return="normalized") == 0.27774888397299874
+    assert sfd(est_mcm1(), est_mcm2(), to_return="normalized") == 0.29238200914015666
+    assert sfd(true_mcm(), est_mcm1(), to_return="both") == (3, 0.21501600287975522)
