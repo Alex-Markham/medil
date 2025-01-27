94d63e6abeca0b0e26929496251ac08ba0ce4dac
Alex Markham <alex.markham@causal.dev>
2025-01-27 15:51:10 +0100

    feat: context-specific dataloader

--- src/medil/models.py
+++ src/medil/models.py
@@ -5,6 +5,7 @@ import pickle
 import warnings
 from datetime import datetime
 from pathlib import Path
+from typing import Iterator, List
 
 import numpy as np
 import numpy.typing as npt
@@ -13,14 +14,14 @@ from numpy.random import default_rng
 from scipy.linalg import norm
 from scipy.optimize import minimize
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler as sc
 from torch.nn.functional import lp_pool2d
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, Subset, TensorDataset
 from tqdm import tqdm
 
 from .ecc_algorithms import find_heuristic_1pc
 from .independence_testing import estimate_UDG
 from .interv_vae import VariationalAutoencoder as InterVAE
+from .interv_vae2 import VariationalAutoencoder as InterVAE2
 from .vae import VariationalAutoencoder
 
 
@@ -66,6 +67,10 @@ class Parameters(object):
             self.weights = np.array([])
             with warnings.catch_warnings(action="ignore"):
                 self.vae = InterVAE(0, 0, 0, 0, 0, 0)
+        elif parameterization == "InterVAE2":
+            self.weights = np.array([])
+            with warnings.catch_warnings(action="ignore"):
+                self.vae = InterVAE2(0, 0, 0, 0, 0, 0)
 
     def __str__(self) -> str:
         return "\n".join(
@@ -693,6 +698,261 @@ class DevMedilInterv(NeuroCausalFactorAnalysis):
 
         # building VAE
         model = InterVAE(
+            num_meas,
+            self.hyperparams["meas_width"],
+            self.hyperparams["meas_depth"],
+            self.hyperparams["num_latent"],
+            self.hyperparams["latent_width"],
+            self.hyperparams["latent_depth"],
+        )
+        model = model.to(self.device)
+        optimizer = torch.optim.AdamW(
+            model.parameters(), lr=self.hyperparams["lr"], weight_decay=1e-5
+        )
+        # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 50, gamma=0.90)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, patience=10, factor=0.5
+        )
+        num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+        self.log(f"Number of parameters: {num_params}")
+
+        # training loop
+        model.train()
+        train_elbo, train_error = [], []
+        valid_elbo, valid_error = [], []
+
+        pbar = tqdm(
+            range(self.hyperparams["num_epochs"]), desc="Training NCFA", unit="epoch"
+        )
+        for idx in pbar:
+            self.log(f"Training on epoch {idx}...")
+            train_lb, train_er, nbatch = 0.0, 0.0, 0
+
+            for batch, _ in train_loader:
+                x_batch = batch[:, :-1]
+                interv_idx_batch = batch[:, -1, None]
+                batch_size = x_batch.shape[0]
+                x_batch = x_batch.to(self.device)
+                recon_batch, logcov_batch, mu_batch, logvar_batch = model(
+                    x_batch, interv_idx_batch
+                )
+                causal_biadj_batch = model.decoder.mean_causal.weight
+                loss = self._elbo_gaussian(
+                    x_batch,
+                    recon_batch,
+                    logcov_batch,
+                    mu_batch,
+                    logvar_batch,
+                    causal_biadj_batch,
+                    self.hyperparams["beta"],
+                )
+                error = self._recon_error(
+                    x_batch, recon_batch, logcov_batch, weighted=False
+                )
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+                # update loss and nbatch
+                train_lb += loss.item() / batch_size
+                train_er += error.item() / batch_size
+                nbatch += 1
+
+            # finish training epoch
+            # scheduler.step()
+            train_lb = train_lb / nbatch
+            train_er = train_er / nbatch
+            train_elbo.append(train_lb)
+            train_error.append(train_er)
+            self.log(f"Finish training epoch {idx} with loss {train_lb}")
+
+            # append validation loss
+            valid_lb, valid_er = self._valid_vae(model, valid_loader)
+            valid_elbo.append(valid_lb)
+            valid_error.append(valid_er)
+
+            # decrease learning rate if validation plateaus
+            scheduler.step(valid_lb)
+
+            # update tqdm progress bar
+            pbar.set_postfix({"loss": train_lb})  # , "validation loss": valid_lb
+
+        train_elbo, train_error = np.array(train_elbo), np.array(train_error)
+        valid_elbo, valid_error = np.array(valid_elbo), np.array(valid_error)
+        elbo = [train_elbo, valid_elbo]
+        error = [train_error, valid_error]
+
+        return model, elbo, error
+
+    def _valid_vae(self, model, valid_loader):
+        """Training VAE with the specified image dataset
+        :param model: trained VAE model
+        :param valid_loader: validation image dataset loader
+        :return: validation loss
+        """
+        # set to evaluation mode
+        model.eval()
+        valid_lb, valid_er, nbatch = 0.0, 0.0, 0
+
+        for batch, _ in valid_loader:
+            x_batch = batch[:, :-1]
+            interv_idx_batch = batch[:, -1, None]
+
+            with torch.no_grad():
+                batch_size = x_batch.shape[0]
+                x_batch = x_batch.to(self.device)
+                recon_batch, logcov_batch, mu_batch, logvar_batch = model(
+                    x_batch, interv_idx_batch
+                )
+                loss = self._elbo_gaussian(
+                    x_batch,
+                    recon_batch,
+                    logcov_batch,
+                    mu_batch,
+                    logvar_batch,
+                    None,
+                    self.hyperparams["beta"],
+                )
+                error = self._recon_error(
+                    x_batch, recon_batch, logcov_batch, weighted=False
+                )
+
+                # update loss and nbatch
+                valid_lb += loss.item() / batch_size
+                valid_er += error.item() / batch_size
+                nbatch += 1
+
+        # report validation loss
+        valid_lb = valid_lb / nbatch
+        valid_er = valid_er / nbatch
+        self.log(f"Finish validation with loss {valid_lb}")
+
+        return valid_lb, valid_er
+
+    def _elbo_gaussian(self, x, x_recon, logcov, mu, logvar, causal_biadj, beta):
+        """Calculating loss for variational autoencoder
+        :param x: original image
+        :param x_recon: reconstruction in the output layer
+        :param logcov: log of covariance matrix of the data distribution
+        :param mu: mean in the fitted variational distribution
+        :param logvar: log of the variance in the variational distribution
+        :param beta: beta
+        :return: reconstruction loss + KL
+        """
+
+        # KL-divergence
+        # https://github.com/AntixK/PyTorch-VAE/blob/master/models/vanilla_vae.py
+        # https://github.com/AntixK/PyTorch-VAE/blob/master/models/beta_vae.py
+        # https://arxiv.org/pdf/1312.6114.pdf
+        kl_div = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        # reconstruction loss
+        cov = torch.exp(logcov)
+        cov = self._apply_along_axis(torch.diag, cov, axis=0)
+        cov = cov.mean(axis=0)
+
+        diff = x - x_recon
+        recon_loss = torch.sum(
+            torch.det(cov)
+            + torch.diagonal(
+                torch.mm(
+                    torch.mm(diff, torch.inverse(cov)), torch.transpose(diff, 0, 1)
+                )
+            )
+        ).mul(-1 / 2)
+
+        # elbo
+        loss = -beta * kl_div + recon_loss
+        if causal_biadj is not None:
+            llambda = self.hyperparams["lambda"]
+            norm_type = 2
+            kernel_size = (
+                self.hyperparams["latent_width"],
+                self.hyperparams["latent_width"],
+            )
+            causal_biadj = causal_biadj[None, None, :, :]
+            causal_biadj = lp_pool2d(
+                causal_biadj, norm_type, kernel_size
+            ).squeeze()  # penalize num edges
+            self.parameters.causal_biadj = causal_biadj.detach().numpy().T
+            return -loss + llambda * causal_biadj.norm(1)
+        return -loss
+
+
+class DevMedilInterv2(NeuroCausalFactorAnalysis):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.parameters = Parameters("InterVAE2")
+        self.hyperparams.update(
+            {
+                "batch_size": 128,
+                "num_epochs": 200,
+                "lr": 0.005,
+                "beta": 1,
+                "num_valid": 1000,
+                "lambda": 0.01,
+                "meas_width": 1,
+                "meas_depth": 0,
+                "num_latent": 5,
+                "latent_width": 1,
+                "latent_depth": 0,
+            }
+        )
+
+    def _data_loader(self, sample):
+        sample_x = sample.astype(np.float32)
+        sample_z = np.empty(shape=(sample_x.shape[0], 0)).astype(np.float32)
+        dataset = TensorDataset(torch.tensor(sample_x), torch.tensor(sample_z))
+        data_loader = DataLoader(
+            dataset,
+            batch_sampler=self._sampler(dataset, self.hyperparams["batch_size"]),
+        )
+        return data_loader
+
+    class _sampler(torch.utils.data.Sampler):
+        def __init__(self, data: List[str], batch_size: int) -> None:
+            self.data = data
+            self.batch_size = batch_size
+            sample_x = data[:][0]
+            self.labels = sample_x[:, -1]
+            self.contexts, self.inv, self.counts = torch.unique(
+                self.labels, return_inverse=True, return_counts=True
+            )
+
+        def __len__(self) -> int:
+            # self.chunk_sizes = [
+            #     (count + self.batch_size - 1) // self.batch_size
+            #     for count in self.counts
+            # ]
+            # return sum(self.chunk_sizes)
+            return (len(self.data) + self.batch_size - 1) // self.batch_size
+
+        def __iter__(self) -> Iterator[List[int]]:
+            context_idcs = torch.multinomial(
+                self.counts / self.counts.sum(), len(self), replacement=True
+            )
+            # contexts = self.contexts[context_idcs]
+            for context_idx in context_idcs:
+                context_data_idcs = torch.where(self.inv == context_idx)[0]
+                batch_idcs = torch.randperm(len(context_data_idcs))[: self.batch_size]
+                yield batch_idcs
+
+    def _train_vae(self, train_loader, valid_loader):
+        """Training VAE with the specified image dataset
+        :param m: dimension of the latent variable
+        :param n: dimension of the observed variable
+        :param train_loader: training image dataset loader
+        :param valid_loader: validation image dataset loader
+        :param biadj_mat: the adjacency matrix of the directed graph
+        :param seed: random seed for the experiments
+        :return: trained model and training loss history
+        """
+
+        num_meas = self.dataset.shape[1] - 1  # one column for interv labels;
+        self.num_meas = num_meas
+
+        # building VAE
+        model = InterVAE2(
             num_meas,
             self.hyperparams["meas_width"],
             self.hyperparams["meas_depth"],
